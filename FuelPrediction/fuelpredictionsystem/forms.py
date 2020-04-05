2a6bda226d9b10f57fcdce81b841a084a839de2f
MD Tanim Hasan <53198840+Tanim419@users.noreply.github.com>
2020-04-05 10:44:46 -0500

    user registration edited

--- FuelPrediction/fuelpredictionsystem/forms.py
+++ FuelPrediction/fuelpredictionsystem/forms.py
@@ -6,21 +6,22 @@ from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
 	email = forms.EmailField()
-	city = forms.CharField(required=True)
-	state = forms.CharField(required=True)
-	phone = forms.CharField(required=True)
+	# city = forms.CharField(required=True)
+	# state = forms.CharField(required=True)
+	# phone = forms.CharField(required=True)
 	class Meta:
 		model = User
-		fields = ["username", "first_name", "last_name", "email", "phone","city", "state", "password1", "password2"]
+		# fields = ["username", "first_name", "last_name", "email", "phone","city", "state", "password1", "password2"]
+		fields = ["username", "first_name", "last_name", "email", "password1", "password2"]
 
 	def save(self, commit=True):
 		user = super(RegisterForm, self).save(commit=False)
 		user.first_name = self.cleaned_data['first_name']
 		user.last_name = self.cleaned_data['last_name']
 		user.email = self.cleaned_data['email']
-		user.phone = self.cleaned_data['phone']
-		user.city = self.cleaned_data["city"]
-		user.state = self.cleaned_data["state"]
+		# user.phone = self.cleaned_data['phone']
+		# user.city = self.cleaned_data["city"]
+		# user.state = self.cleaned_data["state"]
 
 		if commit:
 			user.save()
